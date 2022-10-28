4075ba7d2dd8ac95e0a0658e90b61f3641feecf8
F3865450 Guilherme Moreira Felix <guilherme.felix@bb.com.br>
2022-10-27 22:17:49 -0300

    debug: achando razao nao persistencia

--- src/main/java/gmf/rest/EspectadorController.java
+++ src/main/java/gmf/rest/EspectadorController.java
@@ -64,8 +64,11 @@ public class EspectadorController {
     public Espectador salvar(@RequestBody @Valid Espectador espectador) {
         Usuario user = usuarioRepository.findById(espectador.getUsuario().getId()).orElseThrow();
         user.setIsCadastrado(true);
+        System.out.println(">>>>> vai salvar usuario");
         usuarioRepository.save(user);
+        System.out.println(">>>>> vai salvar endereco");
         enderecoRepository.save(espectador.endereco);
+        System.out.println(">>>>> vai salvar espectador");
         return repository.save(espectador);
     }
 
